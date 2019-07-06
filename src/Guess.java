88935730c51e1b638271f99c5a65852e2daebbb4
Yaroslav <Kaminski.y@mail.ru>
2019-07-06 12:57:20 +0300

    Улучшеная версия 1.1

--- src/Guess.java
+++ src/Guess.java
@@ -9,5 +9,6 @@ public class Guess {
             ch = (char) System.in.read();
 
             if (ch == answer) System.out.println("** You guessed it! **");
+            else System.out.println("... Sorry, you didn't guess :-(");
         }
     }
