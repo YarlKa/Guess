03a5ae4bcdb8669d0b7f348eacaca5e7dc0b28a8
Yaroslav <Kaminski.y@mail.ru>
2019-07-06 12:53:34 +0300

    Initial commit

---
+++ src/Guess.java
@@ -0,0 +1,13 @@
+public class Guess {
+    public static void main(String[] args)
+        throws java.io.IOException {
+            char ch, answer = 'S';
+
+            System.out.println("Guess the letter");
+            System.out.print("Try to guess it: ");
+
+            ch = (char) System.in.read();
+
+            if (ch == answer) System.out.println("** You guessed it! **");
+        }
+    }
